29d3a2028db4149a21fc31fb14cf25f5e5f312cb
honggom <cavok699@naver.com>
2021-07-28 10:24:38 +0900

    init

---
+++ src/main/java/hong/gom/springbootsecuritypractice3/student/StudentAuthenticationToken.java
@@ -0,0 +1,31 @@
+package hong.gom.springbootsecuritypractice3.student;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+
+import java.util.Set;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class StudentAuthenticationToken implements Authentication {
+
+    private Student principal;
+    private String credentials;
+    private String details;
+    private boolean authenticated;
+    private Set<GrantedAuthority> authorities;
+
+
+    @Override
+    public String getName() {
+        return principal == null ? "" : principal.getUsername();
+    }
+
+}
